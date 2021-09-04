a4dcb1e182c9ac83f03845837e248f1587e67939
Matthijs Boet <m.boet@chello.nl>
2021-09-04 20:19:24 +0200

    chapter 4

--- 3-socket-io-101/chat.js
+++ 3-socket-io-101/chat.js
@@ -5,13 +5,20 @@ const app = express();
 app.use(express.static(__dirname + "/public"))
 
 const expressServer = app.listen(9000, console.log(`Listening on http://localhost:9000`))
-const io = socketio(expressServer, {
-	origins: "http://localhost:9000"
-})
+// const io = socketio(expressServer, {
+// 	origins: "http://localhost:9000"
+// })
+
+const io = socketio();
+io.attach(expressServer)
 
 io.on("connection", socket => {
 	socket.emit("messageFromServer", {
 		data: "Welcome to the socketio server"
 	})
 	socket.on("messageFromServer", dataFromClient => console.log(dataFromClient))
+	socket.on("newMessageToServer", msg => {
+		// console.log(msg)
+		io.emit("messageToClients", {text: msg.text})
+	})
 })
